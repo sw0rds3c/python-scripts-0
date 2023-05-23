7ad90904abeeed5012ab297f3ce6d8445f90c280
sw0rds3c <99446293+sw0rds3c@users.noreply.github.com>
2023-05-23 09:54:57 -0700

    Update reverse_function_with_slicing.py

--- reverse_function_with_slicing.py
+++ reverse_function_with_slicing.py
@@ -1,12 +1,7 @@
 #reverse a list using slicing instead of the built-in reverse() function.
 def reverseList(inputList):
-
    reversedList = inputList[::-1]
-
    return reversedList
-
 inputList = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Indigo", "Violet"]
-
 print("The original list is: ", inputList)
-
 print("The list after reversal is: ", reverseList(inputList))
